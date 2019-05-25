bd7e7485b99a61eefcd590dc4867129f3a140f2d
Daniel Barke <dbarke@uci.edu>
2019-05-25 14:40:56 -0700

    rdy for first test run

--- BetterRolls/Macros.py
+++ BetterRolls/Macros.py
@@ -3,30 +3,52 @@ import os
 
 #Form: ,macro name stuff
 #,unmacro name
+MACROPATH = "BetterRolls/macros/macro"
 
 def create(user, list):
-    if(len(list) < 2):
+    if not user:
+        return 1
+    if not list:
         return 1
     cmdKey = user+list[0]
-    cmd=[]
+    '''
+    cmd=""
     for i in range(1, len(list)):
         cmd += list[i]
-    db = shelve.open("BetterRolls/macro.txt")
+        cmd += " "
+    '''
+    cmd = list[1:]
+    print(f'Creating Macro {cmdKey} with data {cmd}')
+    db = shelve.open(MACROPATH)
     db[cmdKey] = cmd
     db.close()
     return 0
 
 def delete(user, cmdName):
+    if not user:
+        return 1
+    if not cmdName:
+        return 1
     cmdKey = user+cmdName
-    db = shelve.open("BetterRolls/macro.txt")
+    print(f'Deleting Macro {cmdKey}')
+    db = shelve.open(MACROPATH)
     flag = cmdKey in db
     if flag:
         del db[cmdKey]
     db.close()
+    return 0
 
 def get(user, cmdName):
+    if not user:
+        return None
+    if not cmdName:
+        return None
     cmdKey = user+cmdName
-    db = shelve.open("BetterRolls/macro.txt")
+    db = shelve.open(MACROPATH)
+    flag = cmdKey in db
+    if not flag:
+        return None
     cmd = db[cmdKey]
+    print(f'Getting Macro {cmdKey} that had data {cmd}')
     db.close()
     return cmd
--- BetterRollsBot.py
+++ BetterRollsBot.py
@@ -4,11 +4,14 @@ import sys
 import errno
 from discord.ext import commands
 from BetterRolls import GodlikeRoll
+from BetterRolls import Macros
 
 TOKEN = 'NTc1NTExODA4MTk1MDM1MTM2.XOHzmQ.4j0mPP3AYqpO1932s_dfo4R5NLk'
 
 #client = discord.Client()
 client = commands.Bot(command_prefix = ',')
+
+#client._skip_check = lambda a,b:False
 
 @client.event
 async def on_ready():
@@ -29,13 +32,7 @@ async def roll(ctx, *argv):
         await ctx.send("Invalid Format! Usage -- r[oll] dice [dice...]\nDice can be formatted as normal(d), hard(h), or wiggle(w) in the format of {Quantity}d{Size}\nFor Normal and Wiggle dice, Size must equal 10 or be left empty.")
         return None
     out = dice.Calc(rolls)
-    #await ctx.send(f'You Rolled {dice.diceList} and have {dice.wiggle} wiggle dice!')
     await ctx.send(out)
-
-@client.command(aliases=['makemacro', 'MAKEMACRO', 'MakeMacro', 'mm', 'MM'])      #could make a command hidden, could give an alias
-async def makeMacro(ctx, *argv):        #ctx stands for context and is sent in automatically
-    usrhash = hash(ctx.message.author)
-    status = create(usrhash, *argv)
 
 
 @client.command(aliases=['exit'])      #could make a command hidden, could give an alias
@@ -43,9 +40,43 @@ async def quit(ctx):        #ctx stands for context and is sent in automatically
     await ctx.send("Goodbye")
     sys.exit(0)
 
-'''
 @client.command()      #could make a command hidden, could give an alias
-async def help(ctx):        #ctx stands for context and is sent in automatically
-    await ctx.send("Current Commands listing: ping, roll, exit")
-'''
+async def test(ctx):        #ctx stands for context and is sent in automatically
+    #ctx.invoke(roll)
+    await ctx.send(",ping")
+
+@client.command(aliases=['makemacro', 'MAKEMACRO', 'MakeMacro', 'mm', 'MM'])      #could make a command hidden, could give an alias
+async def makeMacro(ctx, *argv):        #ctx stands for context and is sent in automatically
+    usrhash = str(hash(ctx.message.author))
+    if ( len(argv) < 2):
+        await ctx.send("Invalid Format! Usage -- ,MakeMacro MacroName Command")
+        return
+    status = Macros.create(usrhash, argv)
+    if status:
+        await ctx.send("Invalid Format! Usage -- ,MakeMacro MacroName Command")
+
+@client.command(aliases=['deletemacro', 'DELETEMACRO', 'DeleteMacro', 'dm', 'DM'])      #could make a command hidden, could give an alias
+async def deleteMacro(ctx, cmdName):        #ctx stands for context and is sent in automatically
+    usrhash = str(hash(ctx.message.author))
+    status = Macros.delete(usrhash, cmdName)
+    if status:
+        await ctx.send("Invalid Format! Usage -- ,DeleteMacro MacroName")
+
+@client.command(aliases=['Macro', 'MACRO', 'm', 'M'])      #could make a command hidden, could give an alias
+async def macro(ctx, cmdName):        #ctx stands for context and is sent in automatically
+    usrhash = str(hash(ctx.message.author))
+    cmd = Macros.get(usrhash, cmdName)
+    if cmd:
+        #await ctx.send(cmd)
+        #async def roll(ctx, *argv):
+        dice = GodlikeRoll.BetterGRoll()
+        rolls = dice.parse(cmd)
+        if(not rolls):
+            await ctx.send("Invalid Format! Usage -- r[oll] dice [dice...]\nDice can be formatted as normal(d), hard(h), or wiggle(w) in the format of {Quantity}d{Size}\nFor Normal and Wiggle dice, Size must equal 10 or be left empty.")
+            return None
+        out = dice.Calc(rolls)
+        await ctx.send(out)
+    else:
+        await ctx.send("Invalid Macro")
+
 client.run(TOKEN)
